4cade68ae8b72a69d81774e37b2f500814c9782f
Shourya Tyagi <shouryatyagi042@gmail.com>
2022-08-10 22:25:17 +0530

    Create ClassBasics.js

---
+++ ClassBasics/ClassBasics.js
@@ -0,0 +1,11 @@
+class Student{
+    constructor(firstName, secondName, year) {
+        this.firstName = firstName ;
+        this.secondName = secondName ;
+        this.year = year ;
+    }
+}
+
+let s1 = new Student("Shourya","Tyagi",3) ;
+
+console.log(s1.firstName) ;
