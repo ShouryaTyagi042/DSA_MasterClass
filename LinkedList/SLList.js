6c425d5b8f7215fbaf279f6018e8430cf104188e
Shourya Tyagi <shouryatyagi042@gmail.com>
2022-08-23 23:44:58 +0530

    pop implemented

--- LinkedList/SLList.js
+++ LinkedList/SLList.js
@@ -22,12 +22,31 @@ class singlyLinkedList {
         }
         this.length++ ;
         return this ;
-
     }
     pop(){
+        if (!this.head) return undefined ;
+        var current = this.head ;
+        var newTail = current ;
+        while(current.next) {
+            newTail = current ;
+            current = current.next ;
+        }
+        this.tail = newTail ;
+        this.tail.next = null ;
+        this.length -- ;
+        if(this.length == 0) {
+            this.tail = null ;
+            this.head = null ;
+        }
+        return current ;
+
+
 
     }
 }
+
+
+
 
 var list = new singlyLinkedList() ;
 list.push("hello") ;
