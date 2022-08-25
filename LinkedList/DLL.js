2a2dbf7e0357e1b6aca084804a1fcda964a93601
Shourya Tyagi <shouryatyagi042@gmail.com>
2022-08-26 00:04:08 +0530

    Update DLL.js

--- LinkedList/DLL.js
+++ LinkedList/DLL.js
@@ -45,13 +45,59 @@ class doublyLinkedList{
         if(this.length == 1) {
             this.head = null ;
             this.tail = this.head ;
+        }else {
+                this.head = shifted.next ;
+                this.head.prev = null ;
+                shifted.next = null ;
         }
-        this.head = shifted.next ;
-        this.head.prev = null ;
-        shifted.next = null ;
         this.length -- ;
         return this;
     }
+    unShift(val) {
+        var newNode = new Node(val) ;
+        if(!this.head) {
+            this.head = newNode ;
+            this.tail = this.head ;
+        }
+        this.head.prev = newNode ;
+        newNode.next = this.head ;
+        this.head  = newNode ;
+        this.length ++ ;
+        return this ;
+    }
+    get(index) {
+        if(index >= this.length || index < 0) {
+            return null ;
+        }
+        if (index <= this.length/2 ) {
+            var counter = 0 ;
+            var current = this.head ;
+            while (counter !== index ) {
+                current = current.next ;
+                counter ++ ;
+            }
+            return current ;
+        }
+        else {
+            var counter = this.length - 1 ;
+            var current = this.tail ;
+            while (counter !==  index) {
+                current = current.prev ;
+                counter -- ;
+            }
+            return current ;
+        }
+
+    }
+    set(index,val) {
+        var foundNode = this.get(index) ;
+        if(foundNode) {
+            foundNode.val = val ;
+            return true ;
+        }
+        return false ;
+    }
+
 }
 
 var list = new doublyLinkedList() ;
