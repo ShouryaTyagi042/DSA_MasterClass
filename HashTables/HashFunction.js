98a983fdd4e4e67e50ac2f417614fd11c0384915
Shourya Tyagi <shouryatyagi042@gmail.com>
2022-09-11 22:29:51 +0530

    Create HashFunction.js

---
+++ HashTables/HashFunction.js
@@ -0,0 +1,10 @@
+function hash(key,arrLen) {
+    let total = 0 ;
+    let wierd_prime = 31 ;
+    for(let i = 0 ; i < Math.min(key.length,100) ; i++) {
+        let char = key[i] ;
+        let value = char.charCodeAt(0) - 96 ;
+        total = (total*wierd_prime) % arrLen ;
+    }
+    return total ;
+}
