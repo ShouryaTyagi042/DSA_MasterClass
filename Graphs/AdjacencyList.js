04c91224e06ea50e2f60787629c1f9a07c4095d3
Shourya Tyagi <shouryatyagi042@gmail.com>
2022-09-13 23:39:37 +0530

    Update AdjacencyList.js

---
+++ Graphs/AdjacencyList.js
@@ -0,0 +1,40 @@
+class graph {
+    constructor() {
+        this.adjacencylist = {} ;
+    }
+    addVertex(vertex) {
+        if(!this.adjacencylist[vertex]) this.adjacencylist[vertex] = [] ;
+    }
+    addEdge(v1,v2) {
+        this.adjacencylist[v1].push(v2) ;
+        this.adjacencylist[v2].push(v1) ;
+    }
+    removeEdge(vertex1,vertex2){
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+            v => v !== vertex2
+        );
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+            v => v !== vertex1
+        );
+    }
+    removeVertex(vertex){
+        while(this.adjacencyList[vertex].length){
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);
+        }
+        delete this.adjacencyList[vertex]
+    }
+}
+
+let g = new graph();
+g.addVertex("Dallas");
+g.addVertex("Tokyo");
+g.addVertex("Aspen");
+g.addVertex("Los Angeles");
+g.addVertex("Hong Kong")
+g.addEdge("Dallas", "Tokyo");
+g.addEdge("Dallas", "Aspen");
+g.addEdge("Hong Kong", "Tokyo");
+g.addEdge("Hong Kong", "Dallas");
+g.addEdge("Los Angeles", "Hong Kong");
+g.addEdge("Los Angeles", "Aspen");
