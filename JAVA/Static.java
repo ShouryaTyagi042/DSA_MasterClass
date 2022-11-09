937fa6772fe960809adc11c1ba9243a8ffcf8a2e
Shourya Tyagi <shouryatyagi042@gmail.com>
2022-11-09 22:42:02 +0530

    added new java files

---
+++ JAVA/Static.java
@@ -0,0 +1,10 @@
+public class Static {
+    static void m1(){
+        System.out.println("calling m1") ;
+    }
+    public static void main(String[] args) {
+
+        m1();
+
+    }
+}
---
+++ JAVA/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Enter your arguements") ;
+        for(String i : args) {
+            System.out.println(i) ;
+        }
+
+    }
+}
